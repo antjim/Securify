1044226131ba8f2739af3d7201df9046386248a6
root <root@tfg.storm>
2018-04-30 13:06:21 -0400

    mejora programa + estructura

--- devOP.py
+++ devOP.py
@@ -17,26 +17,77 @@ class App():
 	def run(self):
 		menu=True
 
+		pre=input("¿Desea realizar el procesado? s/n: ")
+		if(pre=="s"):
+			preprocesado.Menu()
+
 		while(menu):
-			pre=input("¿Desea realizar el procesado? s/n:")
-			if(pre=="s"):
-				print("Instalando JAVA - jre 1.8.0_171")
-				preprocesado.javaInstall()
-				print("[OK] Instalación de Java finalizada.")
+			print("herramientas ...")
+			break
+				#print("Instalando JAVA - jre 1.8.0_171")
+				#preprocesado.javaInstall()
+				#print("[OK] Instalación de Java finalizada.")
+
+	def validaPet(pet):
+		res=True
+		for i in pet:
+			if( (i!="s") and (i != "n") ):
+				res=False
+				break
+		return res
 
 
 
 class preprocesado:
-	def __init__(self,vmaven):
-		self.vmaven=vmaven
-	
+
+	def Menu():
+		obj=[]
+		java=input("¿instalar java con sus respectivas variables? s/n: ")
+		obj.append(java)
+		maven=input("¿Descargar maven y configurarlo? s/n: ")
+		obj.append(maven)
+		
+		os.system("clear")
+		print("Parámetros de configuración seleccionados: Java ["+java+"]"+", "+"Maven ["+maven+"]")
+		continuar=input("¿Continuar con configuración seleccionada? s/n: ")
+		
+		obj.append(continuar)
+		if(App.validaPet(obj)):
+			
+			if(continuar=="s"):
+				preprocesado.gestionaProcesado(java,maven)
+			else:
+				print("Procesado inicial cancelado.")
+
+		else:
+			print("Por favor use 's' o 'n' para seleccionar la correspondiente acción")
+			print("Volviendo al menu principal ...")
+
+
+	def gestionaProcesado(java,maven):
+		if(java == "s"):
+			print("Instalando Java")
+			preprocesado.javaInstall()
+			print("[OK] Instalación Java finalizada")
+		if(maven == "s"):
+			print("Instalando Maven ... ")
+			preprocesado.mavenInstall()
+			print("[OK] Instalación Maven finalizada")
+
 	def javaInstall():
 		os.system("mkdir /usr/java")
-		os.system("tar -zxvf  utilidades/jre1.8.0_171.tar.gz")
-		os.system("mv jre1.8.0_171 /usr/java")
-		os.system("export JAVA_HOME=/usr/java/jre1.8.0_171")
-		os.system("export PATH=$PATH:/usr/java/jre1.8.0_171/bin")
+		os.system("tar -zxvf  utilidades/jre-10.0.1_linux-x64_bin.tar.gz")
+		os.system("mv jre-10.0.1 /usr/java")
+		os.system("export JAVA_HOME=/usr/java/jre-10.0.1")
+		os.system("export PATH=$PATH:/usr/java/jre-10.0.1/bin")
 
+
+	def mavenInstall():
+		os.system("tar -zxvf utilidades/apache-maven-3.5.3-bin.tar.gz")
+		os.system("mv apache-maven-3.5.3 /opt/apache-maven-3.5.3")
+		m1="export MAVEN_HOME=/opt/apache-maven-3.5.3"
+		m2="export M2_HOME=/opt/apache-maven-3.5.3"
+		m3="export PATH=$PATH:$M2_HOME/bin"
 
 # -- fin clases --
 
---
+++ utilidades/maven.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export MAVEN_HOME=/opt/apache-maven-3.5.3
+export M2_HOME=/opt/apache-maven-3.5.3
+export PATH=$PATH:$M2_HOME/bin
+
