a0a25b6238b9470ef26852844013f17d3a99068b
antjim <ant.jim.v@gmail.com>
2018-05-07 20:50:51 +0200

    mejoras, solución de algunos errores, certificados integrados, EN PROCESO - preparación de entorno

--- storm.py
+++ storm.py
@@ -17,7 +17,14 @@ class Storm():
 		menu=True
 		os.system("clear")
 		print(" ")
-		PreEnt.gestionIn()
+		
+		global ro
+		ro=PreEnt.gestionIn()
+
+		rs=ro[0]
+		rz=ro[1]
+
+		PreEnt.preparaEnt(rs,rz)
 
 		while(menu):
 			os.system("clear")
@@ -33,10 +40,10 @@ class Storm():
 			print("===================================")
 			qm=input("Seleccionar una de las opciones: ")
 
-			if(qm==1):
-				Integridad.cfgSSL()
+			if(qm=="1"):
+				Integridad.cfgSSL(rs)
 
-			elif(qm==4):
+			elif(qm=="4"):
 				devOP.contramedidas.Menu()
 
 			else:
@@ -66,6 +73,58 @@ class Storm():
 			except OSError:
 				print("[Error] Sistema operativo soportado para Debian, derivados y CentOS")
 
+	def confStorm(alm,rs):	#lectura del fichero de conf
+		path=rs+"/conf/storm.yaml"
+		
+		f.open(path)
+		g.open(path+".new","w")
+
+		lineaf=f.readlines()
+		f.close()
+
+		for objeto in alm:
+			f.open(path)		
+			linea=f.readline()
+
+			while( linea != lineaf[-1] ):
+
+				work=True
+
+				if(objeto in linea):
+					g.write(linea)
+					cadenas=alm[objeto]
+					
+					for cadena in cadenas:
+						if(cadena == cadenas[-1]):
+							g.write(cadena+"\n\n")
+						else:
+							g.write(cadena+"\n")
+
+					work=False
+
+				elif(work):
+					g.write(linea)
+
+				linea=f.readline()
+
+			g.close()
+			f.close()
+
+			cd="rm "+rs+"/conf/storm.yaml"
+			cd2="mv "+rs+"/conf/storm.yaml.new "+rs+"/conf/storm.yaml"
+			#os.system(cd)
+			#os.system(cd2)
+
+	def compruebaRuta(ruta):
+
+		try:
+			f=open(ruta+"/conf/storm.yaml")
+			return False
+
+		except:
+			print("No existe el directorio Storm en la ruta: "+ruta)
+			return True
+		
 
 class PreEnt:	#prepararemos directorios o descargaremos storm & zookeeper.
 
@@ -78,51 +137,156 @@ class PreEnt:	#prepararemos directorios o descargaremos storm & zookeeper.
 		obj.append(s)
 		obj.append(zk)
 
-		if(validaPet(obj)):
+		ro=[]
+
+		if(Storm.validaPet(obj)):
 			if(s == 's' or s==''):
+				os.system("clear")
 				rs=input("Indique ruta dónde se encuentre Apache Storm: ")
+
+				if( Storm.compruebaRuta(rs)):
+					PreEnt.gestionIn()
+
 			elif(s=='n'):
 				print("Descargando versión apache-storm-1.2.1")
 				os.system("wget http://apache.claz.org/storm/apache-storm-1.2.1/apache-storm-1.2.1.tar.gz")
 				os.system("tar -zxvf apache-storm-1.2.1.tar.gz")
 				os.system("rm apache-storm-1.2.1.tar.gz")
+				os.system("clear")
 				rs=input("Indique ruta dónde desee guardar Apache Storm (por defecto /usr/apache-storm-1.2.1): ")
 
 				if(rs==''):
-					rs="/usr/"
+					rs="/usr/apache-storm-1.2.1"
 
 				cd="mv apache-storm-1.2.1 "+rs
 				os.system(cd)
 
 			if(zk=='s' or zk==''):
+				os.system("clear")
 				rz=input("Indique ruta dónde se encuentre ZooKeeper: ")
+
+				if(Storm.compruebaRuta(rz) == False):
+					PreEnt.gestionIn()
+
 			elif(zk=='n'):
 				print("Descargando zookeeper-3.4.10")
 				os.system("wget http://apache.claz.org/zookeeper/zookeeper-3.4.10/zookeeper-3.4.10.tar.gz")
 				os.system("tar -zxvf zookeeper-3.4.10.tar.gz")
+				os.system("clear")
 				os.system("rm zookeeper-3.4.10.tar.gz")
-				rs=input("Indique ruta dónde desee guardar Apache ZooKeeper (por defecto /usr/zookeeper-3.4.10): ")
+				rz=input("Indique ruta dónde desee guardar Apache ZooKeeper (por defecto /usr/zookeeper-3.4.10): ")
 
 				if(rz==''):
-					rz="/usr/"
+					rz="/usr/zookeeper-3.4.10"
 
 				cd="mv zookeeper-3.4.10 "+rz
 				os.system(cd)
+
+			ro.append(rs)
+			ro.append(rz)
+
+			return ro
+
 		else:
 			print("Por favor use 's'/'INTRO' o 'n'  para seleccionar la correspondiente acción")
 			PreEnt.gestionIn()
 
+	def preparaEnt(rs,rz):	#prepara la configuración inicial de Storm y ZooKeeper
+
+		#configuración Storm
+		path=rs+"/conf/storm.yaml"
+		pathz=rz+"/conf/"
+
+		def entStorm(path):
+
+			f=open(path)
+			g=open(path+".new","w")
+
+			lineaf=f.readlines()
+			f.close()
+
+			f=open(path)
+			linea=f.readline()
+
+			w2=False
+			activa=False
+
+			while(linea != lineaf[-1]):
+				work=True
+
+				if("storm.zookeeper.servers" in linea):
+					if("#" in linea):
+						cadena=linea[1:len(linea)]
+						g.write(cadena)
+					else:
+						g.write(linea)
+
+					w2=True
+					work=False
+					linea=f.readline()
+
+				if( ("-" in linea) and (work==True) and (w2) ):
+					if( ("#" in linea) and (activa==False) ):
+						cadena="      - 'localhost' "+"\n"
+						g.write(cadena)
+						activa=True
+					else:
+						g.write(linea)
+
+					linea=f.readline()
+
+					if(linea != '-'):
+						w2=False
+
+				if("nimbus.seeds: " in linea):
+					if("#" in linea):
+						cadena=" nimbus.seeds: ['localhost']"+"\n"
+						g.write(cadena)
+					else:
+						g.write(linea)
+
+					linea=f.readline()
+
+				if(work):
+					g.write(linea)
+					linea=f.readline()
+
+			f.close()
+			g.close()
+
+		def entZk(path):
+			try:
+				f=open(path)
+				cd="mv "+path+"zoo_sample.cfg"+" "+path+"zoo.cfg"
+				os.system(cd)
+			except:
+				pass
+				#el fichero ya estaba modificado
+
+		entStorm(path)
+		entZk(pathz)
+
 
 class Integridad:	#opciones de integridad para Storm
 
-	def cfgSSL():
-		print("Cooking")
+	def cfgSSL(rs):
 
 		print("Se crearán unos certificados específicos para Storm.")
 		input("Pulsa [ENTER] para continuar.")
-		#os.system("clear")
-		#devOP.contramedidas.Certificados()
+		os.system("clear")
+		devOP.contramedidas.Certificados()
 
+		#input("Creación de certificados finalizado, pulsa [ENTER] para continuar.")
+		claveAlm=input("Introduzca la clave para el almacén de certificados usados: ")
+		clavePr=input("Introduzca la clave privada usada en los certificados: ")
+
+		alm={}	#La clave privada es la que se solicita en el bash, la otra es del almacén
+		#ui
+		alm["nimbus.seeds: "]=["ui.https.port: 8080","ui.https.keystore.type: 'jks'","ui.https.keystore.path: "+"'"+claveAlm+"'","ui.https.keystore.password: "+"'"+clavePr+"'"]	
+		#drpc
+		alm["ui.https.keystore.password: "]=["drpc.https.port: 3774","drpc.https.keystore.type: 'jks'","drpc.https.keystore.path: "+"'"+claveAlm+"'","drpc.https.keystore.password: "+"'"+clavePr+"'"]		
+
+		Storm.confStorm(alm,rs)
 
 if __name__== '__main__':
 	storm=Storm()
